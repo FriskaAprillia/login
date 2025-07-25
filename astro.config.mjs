50cd2621b91daf47c71def1728c57232bb78f123
FriskaAprillia <21421442_friska_a@widyadharma.ac.id>
2025-07-25 07:43:59 +0700

    Build sukses, siap deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/server';
+import vercel from '@astrojs/vercel';
+
 
 export default defineConfig({
   integrations: [react()],
